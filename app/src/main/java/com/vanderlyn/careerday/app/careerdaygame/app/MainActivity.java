c321046e072d81f2823fb5ce9d4e5f6f6a33b40a
Greg Ennis <grennis@gmail.com>
2014-02-23 19:36:51 -0500

    Sound effects

--- app/src/main/java/com/vanderlyn/careerday/app/careerdaygame/app/MainActivity.java
+++ app/src/main/java/com/vanderlyn/careerday/app/careerdaygame/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vanderlyn.careerday.app.careerdaygame.app;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
@@ -23,9 +24,11 @@ public class MainActivity extends ActionBarActivity {
     private float mBirdVelocityX, mBirdVelocityY;
     private float mBirdX, mBirdY;
     private float mPigX, mPigY;
-    private long mLastBirdUpdate;
+    private long mLastBirdUpdate, mLastPigUpdate;
     private float mScreenWidth, mBirdWidth, mPigWidth;
     private float mScreenHieght, mBirdHeight, mPigHeight;
+    private MediaPlayer mBeepSound;
+    private MediaPlayer mBuzzSound;
 
     public MainActivity() {
     }
@@ -39,6 +42,7 @@ public class MainActivity extends ActionBarActivity {
         mPigView = (ImageView)findViewById(R.id.pig);
         mScoreView = (TextView)findViewById(R.id.score);
         mContainer = findViewById(R.id.container);
+        mBeepSound = MediaPlayer.create(this, R.raw.beep);
 
         mGestureDetector = new GestureDetector(this, new FlingGestureDetector());
         mGestureListener = new View.OnTouchListener() {
@@ -83,11 +87,15 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void positionPig() {
-        mPigX = (float)(Math.random() * ((float)mScreenWidth - mPigWidth));
-        mPigY = (float)(Math.random() * ((float)mScreenHieght - mPigHeight));
+        do {
+            mPigX = (float)(Math.random() * ((float)mScreenWidth - mPigWidth));
+            mPigY = (float)(Math.random() * ((float)mScreenHieght - mPigHeight));
+        } while (pigAndBirdOverlap());
+
         mPigView.setX(mPigX);
         mPigView.setY(mPigY);
         mPigView.setVisibility(View.VISIBLE);
+        mLastPigUpdate = System.currentTimeMillis();
     }
 
     private void updateScoreText() {
@@ -97,63 +105,71 @@ public class MainActivity extends ActionBarActivity {
     private class BirdTask extends TimerTask {
         @Override
         public void run() {
-            long now = System.currentTimeMillis();
-            long diff = now - mLastBirdUpdate;
-
-            mBirdX += (mBirdVelocityX * diff) / 100;
-            mBirdY += (mBirdVelocityY * diff) / 100;
-
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    mBirdView.setX(mBirdX);
-                    mBirdView.setY(mBirdY);
+                    updateGame();
                 }
             });
-
-            if (!mTouchedPig && mPigX >= (mBirdX - mPigWidth) && mPigX <= (mBirdX + mBirdWidth) && mPigY >= (mBirdY - mPigY) && mPigY <= (mBirdY + mBirdHeight)) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mScore += 100;
-                        updateScoreText();
-                        mPigView.setVisibility(View.GONE);
-                        mTouchedPig = true;
-                    }
-                });
-            }
-
-            float rate = 1f - (Math.min(diff, 400f) / 400f);
-            mBirdVelocityX = mBirdVelocityX * rate;
-            mBirdVelocityY = mBirdVelocityY * rate;
-
-            if ((mBirdX > (mScreenWidth - mBirdWidth)) || (mBirdX < 0)) {
-                mBirdX = Math.max(1, Math.min(mScreenWidth - mBirdWidth - 1, mBirdX));
-                mBirdVelocityX *= -1;
-            }
-
-            if ((mBirdY > (mScreenHieght - mBirdHeight)) || (mBirdY < 0)) {
-                mBirdY = Math.max(1, Math.min(mScreenHieght - mBirdHeight - 1, mBirdY));
-                mBirdVelocityY *= -1;
-            }
-
-            if ((Math.abs(mBirdVelocityX) < 5) && (Math.abs(mBirdVelocityY) < 5)) {
-                mBirdVelocityX = 0;
-                mBirdVelocityY = 0;
-
-                if (mTouchedPig) {
-                    mTouchedPig = false;
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            positionPig();
-                        }
-                    });
-                }
-            }
-
-            mLastBirdUpdate = now;
         }
     };
+
+    private void updateGame() {
+        long now = System.currentTimeMillis();
+        long diff = now - mLastBirdUpdate;
+
+        mBirdX += (mBirdVelocityX * diff) / 100;
+        mBirdY += (mBirdVelocityY * diff) / 100;
+        mBirdView.setX(mBirdX);
+        mBirdView.setY(mBirdY);
+
+        if (!mTouchedPig && pigAndBirdOverlap()) {
+            mScore += 100;
+            updateScoreText();
+            mPigView.setVisibility(View.GONE);
+            mBeepSound.start();
+            mTouchedPig = true;
+        }
+
+        float rate = 1f - (Math.min(diff, 400f) / 400f);
+        mBirdVelocityX = mBirdVelocityX * rate;
+        mBirdVelocityY = mBirdVelocityY * rate;
+
+        if ((mBirdX > (mScreenWidth - mBirdWidth)) || (mBirdX < 0)) {
+            mBirdX = Math.max(1, Math.min(mScreenWidth - mBirdWidth - 1, mBirdX));
+            mBirdVelocityX *= -1;
+        }
+
+        if ((mBirdY > (mScreenHieght - mBirdHeight)) || (mBirdY < 0)) {
+            mBirdY = Math.max(1, Math.min(mScreenHieght - mBirdHeight - 1, mBirdY));
+            mBirdVelocityY *= -1;
+        }
+
+        if (mBirdVelocityX != 0 && mBirdVelocityY != 0 && (Math.abs(mBirdVelocityX) < 5) && (Math.abs(mBirdVelocityY) < 5)) {
+            mBirdVelocityX = 0;
+            mBirdVelocityY = 0;
+
+            if (mTouchedPig) {
+                mTouchedPig = false;
+
+                positionPig();
+            } else {
+                mScore -= 50;
+                updateScoreText();
+                positionPig();
+            }
+        } else {
+            if (now - mLastPigUpdate > 1500) {
+                mScore -= 10;
+                updateScoreText();
+                positionPig();
+            }
+        }
+
+        mLastBirdUpdate = now;
+    }
+
+    private boolean pigAndBirdOverlap() {
+        return (mPigX >= (mBirdX - mPigWidth)) && (mPigX <= (mBirdX + mBirdWidth)) && (mPigY >= (mBirdY - mPigHeight)) && (mPigY <= (mBirdY + mBirdHeight));
+    }
 }
